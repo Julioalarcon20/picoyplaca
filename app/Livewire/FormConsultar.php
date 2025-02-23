126e09422738c6a3bf4942ea5aa5dc82808aaecd
Julio <julioalarcon19@gmail.com>
2025-02-23 15:32:07 -0500

    Cambios realizados en presentacion de mensajes

--- app/Livewire/FormConsultar.php
+++ app/Livewire/FormConsultar.php
@@ -51,22 +51,25 @@ class FormConsultar extends Component
             $consulta = Horario::whereRaw('LOWER(dia) LIKE ?', [strtolower($diasEnEspanol)])->first();
             // obtengo el ultimo valor de input de formulario
             $obtenerDigito = substr($this->placa, -1);
-            // guardo en una variable el campo de la base de datos tipo array
-            $digitosPlaca= $consulta->num_placa;
             // valido que la consulta no sea null
             if($consulta!=null){
+                // guardo en una variable el campo de la base de datos tipo array
+                $digitosPlaca= $consulta->num_placa;
                 // evaluo si el valor esta dentro del array para hacer la validacion
                 if(in_array($obtenerDigito,$digitosPlaca )){
                     // Valido si el rango de la hora de circulación
+                    // Variable para presentar el rango de horas en mensaje 
+                    $horario = "Horario en la mañana: {$consulta->hora_inicio_dia} a {$consulta->hora_fin_dia} Horario tarde:{$consulta->hora_inicio_tarde} a {$consulta->hora_fin_tarde}";
                     if ($hora >= $consulta->hora_inicio_dia && $hora <= $consulta->hora_fin_dia) {
                         // envio mensajes a la vista
-                        session()->flash('status','No puedes circular');
+
+                        session()->flash('status', "No puedes circular. $horario");
                         return;
                      } 
                      elseif ($hora >= $consulta->hora_inicio_tarde && $hora <= $consulta->hora_fin_tarde) {
-                        session()->flash('status','No puedes circular');
+                        session()->flash('status', "No puedes circular. $horario");
                     }else{
-                        session()->flash('status','No puedes circular');
+                        session()->flash('status','Su carro si puede circular');
                         return;
                     }
                 }else{
@@ -75,12 +78,13 @@ class FormConsultar extends Component
                 }
             }else{
                 // solo entrara en el caso de sabado y domingo que son dias permitidos para circular
-                session()->flash('status','Su carro si puede circular');
+                session()->flash('status','Su carro si puede circular Sabado y Domingo');
                 return;
             }
             
 
         }catch(Exception $e){
+
             session()->flash('status','Ocurrio un problema en su solicitud');
         }
     
